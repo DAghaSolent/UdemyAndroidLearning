2b7866264fc94f3862663a77ea24f3f339f03d65
DannyAgha <danialagha@utilita.co.uk>
2024-04-17 21:16:24 +0100

    Added Spacers and Padding to neatly space out the UI elements within this application.

--- app/src/main/java/com/example/udemyandroidlearning/MainActivity.kt
+++ app/src/main/java/com/example/udemyandroidlearning/MainActivity.kt
@@ -8,7 +8,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -22,6 +26,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.udemyandroidlearning.ui.theme.UdemyAndroidLearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -49,16 +54,19 @@ fun UnitConverter(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
         ) {
-        Text("Unit Converter")
+        Text("Unit Converter", modifier = Modifier.padding(10.dp))
         OutlinedTextField(value = "", onValueChange = {})
-
-        Row{
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(){
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Select")
                 }
             }
+
+            Spacer(modifier = Modifier.width(8.dp))
+
             Box {
                 Button(onClick = { /*TODO*/ }) {
                     Text("Select")
@@ -66,6 +74,7 @@ fun UnitConverter(){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Result: ")
     }
 }
