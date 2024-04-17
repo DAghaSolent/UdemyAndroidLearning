061e615f9c28dfeab0b18400ac35ee8af5c4930b
DannyAgha <danialagha@utilita.co.uk>
2024-04-17 17:36:15 +0100

    Addition of Text Field, Button which includes a Toast message

--- app/src/main/java/com/example/udemyandroidlearning/MainActivity.kt
+++ app/src/main/java/com/example/udemyandroidlearning/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.udemyandroidlearning
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.udemyandroidlearning.ui.theme.UdemyAndroidLearningTheme
 
@@ -22,25 +28,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    UnitConverter()
                 }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter(){
+    Column {
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {})
+
+        Row{
+            val context = LocalContext.current
+            Button(onClick = {Toast.makeText(context,"Thanks For Clicking", Toast.LENGTH_LONG).show()}) {
+                Text("Click Me!")
+            }
+        }
+        Text("Result: ")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UdemyAndroidLearningTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
 }
