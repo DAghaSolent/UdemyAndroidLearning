3d3379d35c5fbf40ddcb6cd09f7e8f8f794284a7
DannyAgha <danialagha@utilita.co.uk>
2024-04-22 12:26:21 +0100

    Addition of Composable Function to display Item name and Quantity to the user

--- app/src/main/java/com/example/udemyandroidlearning/ShoppingList.kt
+++ app/src/main/java/com/example/udemyandroidlearning/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.udemyandroidlearning
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,7 +23,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.udemyandroidlearning.ui.theme.UdemyAndroidLearningTheme
 
 data class ShoppingItem(val id:Int, var name:String, var quantity:Int, var isEditing:Boolean = false)
 
@@ -43,7 +49,7 @@ fun ShoppingListApp(){
             .fillMaxSize()
             .padding(16.dp)){
             items(sItems){
-
+                ShoppingListItem(it, {}, {})
             }
         }
 
@@ -99,5 +105,22 @@ fun ShoppingListApp(){
                     }
                 })
         }
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+){
+    Row(
+        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+            border = BorderStroke(2.dp, Color(0XFF018786)),
+            shape = RoundedCornerShape(20.dp)
+        )
+    ){
+        Text(item.name, modifier = Modifier.padding(8.dp))
+        Text(item.quantity.toString(), modifier = Modifier.padding(8.dp))
     }
 }
